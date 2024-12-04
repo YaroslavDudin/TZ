39896b67389f6d19ad6c8e1dfecc132318e1a2a7
YaroslavDudin <146335539+YaroslavDudin@users.noreply.github.com>
2024-12-04 21:00:05 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const myBtn = document.getElementById("btn-open-modal");
+const modal = document.getElementById("modal");
+
+myBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.style.display = "block";
+  myBtn.style.display = "none";
+});
+
+document.getElementById("btn-close").addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.style.display = "none";
+    myBtn.style.display = "block";
+  
+});
